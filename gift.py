7aa2d7684a6b1759c1e2a09c7c30f669bf90eec0
jeff24 <j2496g@gmail.com>
2022-09-21 22:57:45 -0300

    Gift

---
+++ gift.py
@@ -0,0 +1,10 @@
+import os
+import imageio
+
+filenames = ['team-pic1.png', 'team-pic2.png']
+images = []
+
+for filename in filenames:
+    images.append(imageio.imread(filename))
+
+imageio.mimsave('team.gif', images, duration = 0.5)
